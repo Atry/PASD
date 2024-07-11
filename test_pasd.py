239f353040d1d59fb430bac7a1e16c980d47d51f
Yang, Bo <bo.yang@protonbase.io>
2024-07-11 19:32:40 +0000

    Workaround for torchvision 0.17

--- test_pasd.py
+++ test_pasd.py
@@ -1,3 +1,15 @@
+# Hack to fix a changed import in torchvision 0.17+, which otherwise breaks
+# basicsr; see https://github.com/AUTOMATIC1111/stable-diffusion-webui/issues/13985
+try:
+    import torchvision.transforms.functional_tensor  # noqa: F401
+except ImportError:
+    try:
+        import torchvision.transforms.functional as functional
+        import sys
+        sys.modules["torchvision.transforms.functional_tensor"] = functional
+    except ImportError:
+        pass  # shrug...
+
 import os
 import sys
 import cv2
